2bb9d8ec9631b40001c8fb4e115a46301a0cac62
Tomasz Chabinka <tomaszchabinka@gmail.com>
2024-12-02 22:31:54 +0100

    day -1 solved

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-use rocket::{get, routes};
+use rocket::{get, response::Redirect, routes, uri};
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "Hello, bird!"
+}
+
+#[get("/-1/seek")]
+fn seek() -> Redirect {
+    Redirect::found(uri!("https://www.youtube.com/watch?v=9Gc4QTqslN4"))
 }
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index]);
+    let rocket = rocket::build().mount("/", routes![index, seek]);
 
     Ok(rocket.into())
 }
